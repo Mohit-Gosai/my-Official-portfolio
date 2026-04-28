cd2ed6d6119d08c5bfc8f55c0129862b393fb512
mohit-gosai <mohitgosai.officle106@gmail.com>
2026-04-28 09:14:22 +0530

    fixed imports

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './Components/Footer'
 import Hero from './Components/Hero'
 import About from './Components/AboutSection'
 import Skills from './Components/Skills'
-import Projects from './Components/projects'
+import Projects from './Components/Projects'
 import Contact from './Components/Contact'
  
 export default function App() {
