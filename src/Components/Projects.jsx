1af2a12695152e70260003b3c1d9e2579e7514ec
mohit-gosai <mohitgosai.officle106@gmail.com>
2026-04-29 09:11:21 +0530

    updated project loading animation

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ProjectCard from './ProjectCard';
+import { motion } from 'framer-motion';
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
@@ -21,7 +22,6 @@ const Projects = () => {
     fetchProjects();
   }, []);
 
-  if (loading) return <div className="text-center py-20 text-white">Loading...</div>;
 
   return (
     <section id="projects" className="bg-transparent py-24 px-6 md:px-16 border-t border-gray-900">
@@ -33,11 +33,20 @@ const Projects = () => {
           <div className="h-1 w-20 bg-rose-900 mt-4"></div>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {projects.map((project, index) => (
-            <ProjectCard key={project._id} project={project} index={index} />
-          ))}
-        </div>
+        {
+          loading ? <motion.div
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: Infinity, ease: "easeOut" }}
+          className="text-center py-20 text-slate font-bold">Loading...</motion.div> :
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+              {
+                projects.map((project, index) => (
+                  <ProjectCard key={project._id} project={project} index={index} />
+                ))
+              }
+            </div>
+        }
       </div>
     </section>
   );
