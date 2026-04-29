afc7f15f9166f2654a030cd05bd3a813b72a3934
mohit-gosai <mohitgosai.officle106@gmail.com>
2026-04-29 09:20:05 +0530

    updated project fixed imports

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ProjectCard from './ProjectCard';
 import { motion } from 'framer-motion';
+import ProjectSkeleton from './ProjectSkeleton';
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
